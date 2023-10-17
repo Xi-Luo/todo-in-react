0897f518ae77ce12dd61033abc7c6083f2a50823
罗玉珠 <luoyuzhu1@crdigital.com.cn>
2023-10-17 18:09:34 +0800

    页面组件初始化

--- src/components/todoList.js
+++ src/components/todoList.js
@@ -1,20 +1,21 @@
-function TodoListItem() {
+function TodoListItem({todoItem}) {
     return (
-        <div class="todoListItem">
-            <div class="todoListItemContent"></div>
-            <div class="todoListItemFoot">
-                <div class="todoListItemTime"></div>
-                <div class="todoListItemDone">Done</div>
+        <div className="todo-list-item" >
+            <div className="todo-list-item-content" >{ todoItem.content}</div>
+            <div className="todo-list-item__foot" >
+                <div className="todo-list-item-time" >{ todoItem.createTime}</div>
+                <div className="todo-list-item-done" >Done</div>
             </div>
         </div>
     )
 }
 
 export default function TodoList() {
+    const todoList = [{content:'this is a todo task',createTime:'2020-02-02 12:22'}]
     return (
         <div>
-            <div class="todoListTitle">TodoList</div>
-            <TodoListItem></TodoListItem>
+            <div className="todo-list-title">TodoList</div>
+            <TodoListItem todoItem={ todoList[0]}></TodoListItem>
         </div>
     )
 }
