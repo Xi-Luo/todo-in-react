c6d79897c93c348cf58da2089b51c24ec5f24ca5
罗玉珠 <luoyuzhu1@crdigital.com.cn>
2023-10-19 18:46:37 +0800

    渲染list

--- src/components/todoList.js
+++ src/components/todoList.js
@@ -4,18 +4,23 @@ function TodoListItem({todoItem}) {
             <div className="todo-list-item-content" >{ todoItem.content}</div>
             <div className="todo-list-item__foot" >
                 <div className="todo-list-item-time" >{ todoItem.createTime}</div>
-                <div className="todo-list-item-done" >Done</div>
+                <div className="todo-list-item-done" >{todoItem.isDone?'✔':'Done'}</div>
             </div>
         </div>
     )
 }
 
 export default function TodoList() {
-    const todoList = [{content:'this is a todo task',createTime:'2020-02-02 12:22'}]
+    const todoData = [
+        { id:1,content: 'this is a todo task', createTime: '2020-02-02 12:22',isDone:true },
+        { id:2,content: 'todo2', createTime: '2023-01-01 11:11', isDone: false },
+        { id:3,content: 'todo2', createTime: '2023-01-01 11:11', isDone: false },
+    ]
+    const todoList = todoData.map(todo => <TodoListItem key={todo.id} todoItem={ todo}></TodoListItem>)
     return (
         <div>
             <div className="todo-list-title">TodoList</div>
-            <TodoListItem todoItem={ todoList[0]}></TodoListItem>
+            {todoList}
         </div>
     )
 }
