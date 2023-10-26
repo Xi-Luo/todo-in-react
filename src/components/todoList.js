d96195e971b2835952bde8fac09bf44fd702f617
罗玉珠 <luoyuzhu1@crdigital.com.cn>
2023-10-26 17:46:58 +0800

    样式修改

--- src/components/todoList.js
+++ src/components/todoList.js
@@ -1,34 +1,28 @@
 "use client"
+import { formatTime } from "@/utils/commonFunc"
 
-import {useState} from 'react'
+
 function TodoListItem({ todoItem, changeDone }) {
     return (
         <div className="todo-list-item" >
-            <div className="todo-list-item-content" >{ todoItem.content}</div>
-            <div className="todo-list-item__foot" >
-                <div className="todo-list-item-time" >{ todoItem.createTime}</div>
-                <div className="todo-list-item-done" onClick={changeDone}>{todoItem.isDone?'✔':'Done'}</div>
+            <div className="todo-list-item-content" >{todoItem.content}</div>
+            <div className="todo-list-item__foot flex justify-between items-center" >
+                <div className="todo-list-item-time" >{formatTime(todoItem.createTime)}</div>
+                <div className="todo-list-item-done " onClick={changeDone}>{todoItem.isDone ? '✔' : 'Done'}</div>
             </div>
         </div>
     )
 }
 
-export default function TodoList() {
-    const [todoData,setTodoData] = useState([
-        { id:1,content: 'this is a todo task', createTime: '2020-02-02 12:22',isDone:true },
-        { id:2,content: 'todo2', createTime: '2023-01-01 11:11', isDone: false },
-        { id:3,content: 'todo2', createTime: '2023-01-01 11:11', isDone: false },
-    ])
-    function changeDone(index) {
-        todoData[index].isDone = !todoData[index].isDone
-        const list = [...todoData]
-        setTodoData(list)
-    }
-    const todoList = todoData.map((todo,index) => <TodoListItem key={todo.id} todoItem={ todo} changeDone={()=>changeDone(index)}></TodoListItem>)
+export default function TodoList({ todoData, changeDone }) {
+    const todoList = todoData.map((todo, index) => <TodoListItem key={todo.id} todoItem={todo} changeDone={() => changeDone(index)}></TodoListItem>)
     return (
-        <div>
+        <div className="todo-list">
             <div className="todo-list-title">TodoList</div>
-            {todoList}
+            <div className="todo-list-body">
+                {todoList}
+            </div>
+
         </div>
     )
 }
