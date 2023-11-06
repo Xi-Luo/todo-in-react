5907e2e05018bdbcaa79c53d1ac377c03ad07583
罗玉珠 <luoyuzhu1@crdigital.com.cn>
2023-11-06 17:02:34 +0800

    增加接口

--- src/api/todo.js
+++ src/api/todo.js
@@ -1,7 +1,13 @@
 import axios from "axios";
 
-const getTodoList = () => axios.get('')
+const getTodoList = () => axios.get('/todo/list')
+const removeTodo = () => axios.post('/todo/remove')
+const addTodo = () => axios.post('/todo/add')
+const updateTodo = () => axios.post('/todo/update')
 
 export {
-	getTodoList
+	getTodoList,
+	addTodo,
+	removeTodo,
+	updateTodo
 }
